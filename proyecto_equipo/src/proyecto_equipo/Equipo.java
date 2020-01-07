325c2e05242cde99c10d7293ccc47e22d30970af
german.gonzalez <german.gonzalez@1gggarzonX650G1.babel-si.local>
2020-01-07 12:44:51 +0100

    Commit definitivo de A

--- proyecto_equipo/src/proyecto_equipo/Equipo.java
+++ proyecto_equipo/src/proyecto_equipo/Equipo.java
@@ -4,7 +4,7 @@ public class Equipo {
 
 	public static void main(String[] args) {
 		System.out.println("Hola Andrea, soy German");
-		System.out.println("Trabajo finalizado. Somos la caña!");
+		
 	}
 
 }
